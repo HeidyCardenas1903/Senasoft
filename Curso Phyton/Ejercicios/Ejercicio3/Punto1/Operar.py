44f5645db0452c37867aa04ffe9a9dfa60998111
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2023-07-31 12:34:33 -0500

    ‎

---
+++ Curso Phyton/Ejercicios/Ejercicio3/Punto1/Operar.py
@@ -0,0 +1,7 @@
+#1.Cree un modulo con todas las operaciones matemáticas, luego impórtela en un archivo.py y realice las operaciones matemáticas llamando a cada función.
+from operaciones_matematicas import *
+
+suma(10,6)
+resta(20,8)
+division(2345,10)
+multiplicacion(365,5)
--- Curso Phyton/Ejercicios/Ejercicio3/Punto1/operaciones_matematicas.py
+++ Curso Phyton/Ejercicios/Ejercicio3/Punto1/operaciones_matematicas.py
@@ -1,14 +1,13 @@
 #1.Cree un modulo con todas las operaciones matemáticas, luego impórtela en un archivo.py y realice las operaciones matemáticas llamando a cada función.
 def suma(a,b):
-    return a + b
+    print(f'El resultado de tu suma es: {a + b}')
 
 def resta(a,b):
-    return a -b
+     print(f'El resultado de tu resta es: {a - b}')
 
 def division(a,b):
-    if b==0:
-        raise ValueError('No es posible dividir entre cero :c')
+     print(f'El resultado de tu division es: {a / b}')
 
 def multiplicacion(a,b):
-    return a * b
+     print(f'El resultado de tu multiplicacion es: {a * b}')
 
