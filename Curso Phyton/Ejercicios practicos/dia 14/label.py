00daa123b6c1fc388292c0458f2bae7e93b6457a
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2023-08-06 14:10:22 -0500

    ‎

--- Curso Phyton/Ejercicios practicos/dia 14/label.py
+++ Curso Phyton/Ejercicios practicos/dia 14/label.py
@@ -2,11 +2,21 @@ from tkinter import *
 
 
 root=Tk()
-root.title("Bienvenida profe Angelica")
+root.title("Bienvenida")
 root.config(width=400, height=300)
 
 label = Label(root,text="Hola Mundo")
-label.pack()
+label.place(x=100,y=50)
+label.config(bg="Pink", fg="Blue", font=("Curier", 20))
+
+label = Label(root,text="Bievenida Profe Angelica")
+label.place(x=100,y=100)
+label.config(bg="Blue", fg="Pink")
+
+
+
+
+
 
 
 
