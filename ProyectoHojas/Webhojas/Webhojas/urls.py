0dbe49a14cc9048668d182e3698f55378ede1576
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2023-08-12 22:28:09 -0500

    ‎

--- ProyectoHojas/Webhojas/Webhojas/urls.py
+++ ProyectoHojas/Webhojas/Webhojas/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for Webhojas project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from . import wiews
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('crear-venta/', wiews.crear_venta, name='crear_venta'),
+    path('listar-ventas/', wiews.listar_ventas, name='listar_ventas'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('algoritmia.urls')),
 ]
