e4ab199f30f41490a3a5cdaca5e10dc4ea0b31a6
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2023-08-14 16:55:26 -0500

    Estructuarcion de la AppHojas

--- ProyectoHojas/AppHojas.py
+++ ProyectoHojas/AppHojas.py
@@ -1,21 +1,22 @@
-import tkinter as tk
+from tkinter import *
 from sqlite3 import connect
+import tkinter as tk
 
-# Creacion de la base de datos / Hojas - Heidy
-#Conexion :)
-conexion = connect("ventas.db")
-cursor = conexion.cursor()
+# # Creacion de la base de datos / Hojas - Heidy
+# #Conexion :)
+# conexion = connect("ventas.db")
+# cursor = conexion.cursor()
 
-#Tabla de ventas
-cursor.execute('''
-CREATE TABLE ventas (
-  nombre text,
-  tipo int,
-  cantidad int,
-  precio_por_hoja float,
-  total float
-)
-''')
+# #Tabla de ventas
+# cursor.execute('''
+# CREATE TABLE ventas (
+#   nombre text,
+#   tipo int,
+#   cantidad int,
+#   precio_por_hoja float,
+#   total float
+# )
+# ''')
 
 #Creacion de la funcion que se encargara de calcular el precio total y el recibo de una venta
 def calcular_total(nombre, tipo_cliente, cantidad, precio_por_hoja):
@@ -31,10 +32,22 @@ def calcular_total(nombre, tipo_cliente, cantidad, precio_por_hoja):
     
     imprimir_recibo(nombre, subtotal, neto_por_pagar)
 
-def print_receipt(name, subtotal, net_total):
-  print("Name of the client:", name)
-  print("Subtotal to pay:", subtotal)
-  print("Net to pay:", net_total)
+def imprimir_recibo(nombre, subtotal, neto_por_pagar):
+    print("Nombre del cliente:", nombre)
+    print("Subtotal por pagar:", subtotal)
+    print("Neto por pagar:", neto_por_pagar)
 
 #App
+window = tk.Tk()
+window.resizable(1,1)
+window.title("App || Hojas de hielo")
+window.iconbitmap("C:\\Users\\hvcar\\OneDrive\\Documentos\\PRACTICAS\\SENASOFT\\ProyectoHojas\\bolsa-de-hielo.ico\\")
+
+nombre_label = tk.Label(text="Nombre del cliente:")
+nombre_label.grid(row=0, column=0)
+
+
+
+
+window.mainloop()
 
