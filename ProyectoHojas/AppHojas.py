641cb9e61592246810285b36c35208ab148ebdae
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2023-08-14 16:08:11 -0500

    Creacion de las tabla y el documento de la DB

--- ProyectoHojas/AppHojas.py
+++ ProyectoHojas/AppHojas.py
@@ -2,4 +2,17 @@ import tkinter as tk
 from sqlite3 import connect
 
 # Creacion de la base de datos / Hojas - Heidy
+#Conexion :)
+conexion = connect("ventas.db")
+cursor = conexion.cursor()
 
+#Tabla de ventas
+cursor.execute('''
+CREATE TABLE sales (
+  name text,
+  type_id int,
+  quantity int,
+  price_per_sheet float,
+  total float
+)
+''')
